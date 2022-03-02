eb5c845075fb0617a43bc9a060cbc5717698d65a
Jamaguana <juanfernando1693@gmail.com>
2022-03-02 17:40:08 -0500

    term changes

--- wp-content/themes/PromoTerpel/term.php
+++ wp-content/themes/PromoTerpel/term.php
@@ -6,17 +6,22 @@ get_header();
 
 $stations = [
     [ "name" => "EDS LO JUSTO", "city" => "Ambato", "address" => "Av.  Bolivariana 747 E Isidro Viteri" ],
-    [ "name" => "EDS SILVAN 2", "city" => "Babahoyo", "address" => "Via Babahoyo" ],
-    [ "name" => "EDS SILVAN 1", "city" => "Babahoyo", "address" => "Los Rios Babahoyo Parroquia Clemente Baquerizo" ],
+    //[ "name" => "EDS SILVAN 2", "city" => "Babahoyo", "address" => "Via Babahoyo" ],
+    //[ "name" => "EDS SILVAN 1", "city" => "Babahoyo", "address" => "Los Rios Babahoyo Parroquia Clemente Baquerizo" ],
     [ "name" => "EDS GAPAL", "city" => "Cuenca", "address" => "Av. 24 De Mayo y Cajabamba" ],
     [ "name" => "EDS NARANCAY", "city" => "Cuenca", "address" => "Panamericana Sur Via Baños Km 1 1-2" ],
     [ "name" => "EDS BAÑOS", "city" => "Cuenca", "address" => "Camino a Baños Av. Las Américas " ],
     [ "name" => "EDS SAN JOSÉ DE TAMARINDO", "city" => "Cuenca", "address" => "Molleturo km 107 Sector Tamarindo Parroquia Jesús María" ],
+
     [ "name" => "EDS ELOY ALFARO", "city" => "Durán", "address" => "Km 2.5 Durán Boliche" ],
+
     [ "name" => "EDS VALGAS", "city" => "El Carmen", "address" => "Km 36 Via Chone" ],
+
     [ "name" => "EDS RIO BONITO", "city" => "El Guabo", "address" => "Panamericana, Via Guayaquil " ],
+
     [ "name" => "EDS EL JARDIÍN", "city" => "Esmeraldas", "address" => "Sur de la Ciudad Redonda De Codesa" ],
     [ "name" => "EDS LA ISLA", "city" => "Esmeraldas", "address" => "Cl 16 Sl 1 Mz 23 Carretero Via Isla" ],
+
     [ "name" => "EDS LA GARZOTA", "city" => "Guayaquil", "address" => "Av. Guillermo Pareja Y Av. Hermano Miguel" ],
     [ "name" => "EDS GARITA CHIMBORAZO", "city" => "Guayaquil", "address" => "Av. Quito Y Primero De Mayo" ],
     [ "name" => "EDS BAHÍA NORTE", "city" => "Guayaquil", "address" => "Av. De Las Américas Solar 5-6 " ],
@@ -29,7 +34,9 @@ $stations = [
     [ "name" => "EDS EL FORTÍN", "city" => "Guayaquil", "address" => "Km 27 Via Perimetral" ],
     [ "name" => "EDS BELLAVISTA", "city" => "Guayaquil", "address" => "Av. Velasco Ibarra No. 3-4-8-9 Y Av. Barcelona" ],
     [ "name" => "EDS PERIMETRAL", "city" => "Guayaquil", "address" => "Av. Perimetral Km 25-Guayaquil" ],
-    [ "name" => "EDS AUTOSERVICIO SUR", "city" => "Guayaquil", "address" => "Kennedy Norte y Av Francisco De Orellana" ],
+    // cambio
+    [ "name" => "EDS AUTOSERVICIO SUR", "city" => "Quito", "address" => "Av Maldonado S10-84 Y Calvas" ],
+
     [ "name" => "EDS EL ÉXITO", "city" => "Guayaquil", "address" => "Aguirre 1621 y Av. Del Ejercito" ],
     [ "name" => "EDS DOMINGO COMIN", "city" => "Guayaquil", "address" => "Av. Domingo Comin Intersección Ernes" ],
     [ "name" => "EDS ALBERESE", "city" => "Guayaquil", "address" => "Via Daule Mz 73 Si 1" ],
@@ -39,28 +46,42 @@ $stations = [
     [ "name" => "EDS ARUAL", "city" => "Guayaquil", "address" => "Via Perimetral Km 42" ],
     [ "name" => "EDS PERIMETRAL", "city" => "Guayaquil", "address" => "Av. Perimetral Km 26" ],
     [ "name" => "EDS PISONI", "city" => "Guayaquil", "address" => "Puente Alterno Norte" ],
+
     [ "name" => "EDS MALACATOS", "city" => "Loja", "address" => "Via Loja Malacatos Km 26" ],
+
     [ "name" => "EDS MACHALA DOS", "city" => "Machala", "address" => "Km. 0.5 25 De Junio" ],
+
     [ "name" => "EDS MALECÓN", "city" => "Manta", "address" => "N° 110 Carretero A Portoviejo" ],
     [ "name" => "EDS AMCO", "city" => "Manta", "address" => "Av. 4 De Noviembre Manta Manabí" ],
+
     [ "name" => "EDS MONTECRISTI", "city" => "Montecristi", "address" => "Parroquia Anibal San Andrés Carrete" ],
+
     [ "name" => "EDS COSTA NORTE", "city" => "Pedernales", "address" => "García Moreno y 27 De Noviembre" ],
+
     [ "name" => "EDS POSOIL", "city" => "Posorja", "address" => "Vial al Puerto de Aguas Profundas" ],
+
     [ "name" => "EDS AUTOCENTRO", "city" => "Quevedo", "address" => "Av. Walter Andrade Cl 4" ],
     [ "name" => "EDS LA VICTORIA", "city" => "Quevedo", "address" => "Quevedo, Km 1 Via San Carlos" ],
+
     [ "name" => "EDS MIRAVALLE", "city" => "Quito", "address" => "Av. Interoceánica L.6 Via a Miravalle" ],
     [ "name" => "EDS ANDINA", "city" => "Quito", "address" => "Autopista General Rumiñahui Lt 424 junto al puente 9" ],
     [ "name" => "EDS TUFIÑO", "city" => "Quito", "address" => "Av. Galo Plaza Lasso Y Av. Luis Tufiño" ],
     [ "name" => "EDS TERPEL UNO", "city" => "Quito", "address" => "Av. 10 De Agosto Y Rio Cofanes" ],
     [ "name" => "EDS VIRGEN DEL QUINCHE", "city" => "Quito", "address" => "Via a Puellaro a 2 cuadras del parque recreacional Jerusalem" ],
     [ "name" => "EDS EL QUINCHE", "city" => "Quito", "address" => "Panamericana Norte Km 2 Y Cornelio" ],
+
     [ "name" => "EDS SENSACIÓN", "city" => "Riobamba", "address" => "Panamericana Norte Y Rio Quevedo" ],
+
     [ "name" => "EDS PRESTOSERVICIO DEL VALLE", "city" => "Ruminahui", "address" => "Av. General Rumiñahui y 7Ma Transversal Conocoto Quito" ],
     [ "name" => "EDS SAN SEBASTIÁN", "city" => "Ruminahui - Salgolqui", "address" => "Av. Luis Cordero Y García Morenos" ],
+
     [ "name" => "EDS LA T SALITRE", "city" => "Salitre", "address" => "Sector la T de Salitre Carretero Via Daule" ],
+
     [ "name" => "EDS FLAMINGO OCCIDENTAL", "city" => "San Lorenzo", "address" => "Panamericana Sur Tambillo" ],
     [ "name" => "EDS FLAMINGO ORIENTAL", "city" => "San Lorenzo", "address" => "Panamericana Sur Tambillo" ],
-    [ "name" => "EDS QUATTRO", "city" => "Santa Elena", "address" => "Via Salinas-La Libertad Cl 53" ],
+
+    [ "name" => "EDS SANTA ELENA", "city" => "Santa Elena", "address" => "Via Salinas-La Libertad Cl 53" ],
+
     [ "name" => "EDS R Y G", "city" => "Santo Domingo De Los Tsáchilas", "address" => "Av. De Los Colonos " ],
     [ "name" => "EDS ANTURIOS I", "city" => "Santo Domingo De Los Tsáchilas", "address" => "Via Quevedo Km 4.5 Margen Izquierdo" ],
     [ "name" => "EDS TRUCK STOP", "city" => "Santo Domingo De Los Tsáchilas", "address" => "Via Quevedo Km 5 Cooperativa Nueva" ],
@@ -77,12 +98,10 @@ $stations = [
                 <img style="width: 100%;" src="<?php bloginfo('template_directory'); ?>/assets/images/banner-terms.jpg" alt="terpelTyC" />
                 <br>
                 <p class="text-center"><span>Bases de la promoción</span></p>
-                <p><span>ORGANIZADOR:</span></p>
-                <p>Terpel-Comercial Ecuador Cia. Ltda.</p>
-                 <p><span>NOMBRE DE LA CAMPAÑA:</span></p>
-                 <p>Tanquea y Viaja a Qatar 2022</p>
-                <p><span>TERRITORIO:</span></p>
-                <p>Esta promoción estará vigente en las estaciones de servicio participantes, las cuáles son: </p>
+                <p><span>ORGANIZADOR:</span> Terpel-Comercial Ecuador Cia. Ltda. </p>
+                 <p><span>NOMBRE DE LA CAMPAÑA:</span> Tanquea y Viaja a Qatar 2022</p>
+                <p><span>TERRITORIO:</span> Esta promoción estará vigente en las estaciones de servicio participantes, las cuáles son: </p>
+            
                 <table>
                     <tbody>
                     <?php foreach($stations  as $x => $val) { ?>
@@ -95,27 +114,28 @@ $stations = [
                     </tbody>
                 </table>
                 <br>
-                <p><span>PARTICIPANTES:</span>Los participantes pueden ser personas mayores de edad de tengan su domicilio en la República del Ecuador y que cumplan con los requisitos de las bases del concurso.
+                <p><span>PARTICIPANTES:</span>
+                Los participantes pueden ser personas mayores de edad de tengan su domicilio en la República del Ecuador y que cumplan con los requisitos de las bases del concurso. 
                 </p>
                 <p>No podrán participar en esta promoción las siguientes personas: </p>
-                <p>(a) Los empleados del Organizador y de los Distribuidores y Operadores de las Estaciones de Servicio; (a) El personal de las agencias de publicidad de la empresa; (b) Los familiares de los empleados del Organizador, de los Distribuidores y Operadores de las Estaciones de Servicio Terpel hasta el segundo grado de consanguineidad y primer grado de afinidad, (c) el personal de las de las agencias vinculadas con esta promoción hasta el segundo grado de consanguineidad y primer grado de afinidad.</p>
-                <p><span>VIGENCIA:</span>La presente promoción estará vigente desde las 00h00 del martes 1 de marzo de 2022 al viernes 2 de septiembre 2022, para participar en la promoción los clientes pueden ingresar facturas de todas las estaciones de servicio de Terpel con un consumo mínimo de 10 USD
+                <p>(a) Los empleados del Organizador y de los Distribuidores y Operadores de las Estaciones de Servicio; (a) El personal de las agencias de publicidad de la empresa; (b) Los familiares de los empleados del Organizador, de los Distribuidores y Operadores de las Estaciones de Servicio Terpel hasta el segundo grado de consanguineidad y primer grado de afinidad, (c) el personal de las de las agencias vinculadas con esta promoción hasta el segundo grado de consanguineidad y primer grado de afinidad</p>
+                <p><span>VIGENCIA:</span> La presente promoción estará vigente desde las 00h00 del martes 1 de marzo de 2022 al viernes 2 de septiembre 2022, para participar en la promoción los clientes pueden ingresar facturas de todas las estaciones de servicio de Terpel con un consumo mínimo de 10 USD.
                 </p>
                 <p><span>MECÁNICA: </span></p>
                 <ol>
                     <li>Ingresar a la página oficial de la promoción http://terpelsicumple.com</li>
                     <li>Ingresar datos personales cédula, nombre, apellido, teléfono, ciudad, mail.</li>
-                    <li>Registrar ciudad, número de factura y monto.</li>
+                    <li>Número de factura, monto, ciudad y estación de servicio participante. </li>
                     <li>Subir foto de la factura registrada o pdf de la misma enviada vía correo electrónico.</li>
                     <li>Aceptar las condiciones generales.</li>
                     <li>Registrar su forma de pago</li>
                     <li>Recibe un (1) cupón por cada 10 USD de consumo</li>
-                    <li>Recibe tripe (3) cupón por cada 10 USD de consumo con Dinners</li>
                     <li>Recibe doble (2) cupón por cada 10 USD de consumo de EVOL-T, compra de Lubricantes Mobil y compras en tiendas Altoque</li>
+                    <li>Recibe tripe (3) cupón por cada 10 USD de consumo con Diners Club, Visa Itanium y Discover</li>
                 </ol>
                 <p><span>SORTEO: </span></p>
                 <p>
-                El sorteo se realizará mediante un sorteo en una plataforma web de selección aleatoria, la elección de los ganadores se hará en presencia de un notario público y un representante de Terpel . con la presencia de un Notario Público, los días 29 de MARZO 2022, 15 DE ABRIL  2022, 29 DE ABRIL DE 2022, 13 DE MAYO 2022, 27 DE MAYO 2022, 10 DE JUNIO 2022, 24 DE JUNIO 2022, 8 DE JULIO 2022, 20 DE JULIO 2022, 5 DE AGOSTO 2022, 19 DE AGOSTO 2022, 2 DE SEPTIEMBRE 2022, el cuál será retransmitido por las redes sociales de Terpel Ecuador por streaming, para cada sorteo se sacarán 5 ganadores suplentes en caso de que el ganador  principal, de cada sorteo  no cumpla con los requisitos y validez del mismo.
+                El sorteo se realizará mediante una plataforma web de selección aleatoria, la elección de los ganadores se hará en presencia de un notario público y un representante de Terpel .Los días 29 de MARZO 2022, 15 DE ABRIL  2022, 29 DE ABRIL DE 2022, 13 DE MAYO 2022, 27 DE MAYO 2022, 10 DE JUNIO 2022, 24 DE JUNIO 2022, 8 DE JULIO 2022, 20 DE JULIO 2022, 5 DE AGOSTO 2022, 19 DE AGOSTO 2022, 2 DE SEPTIEMBRE 2022, el cuál será retransmitido por las redes sociales de Terpel Ecuador por streaming, para cada sorteo se sacarán 5 ganadores suplentes en caso de que el ganador  principal, de cada sorteo  no cumpla con los requisitos y validez del mismo.
                 </p>
 
                 <p><strong>VIGENCIA DE FACTURAS:</strong></p>
@@ -124,7 +144,7 @@ $stations = [
                 </p>
                 
                 <p><strong>GANADORES:</strong></p>
-                <p>Terpel Ecuador se reserva el derecho de verificar la información proporcionada por los participantes y la autenticidad de la factura participante. Por lo tanto, no participarán de la promoción aquellas facturas que se encuentren dañadas, rotas, incompletas, con cualquier tipo de enmendadura externa y/o falsificados, y en general las facturas cuya autenticidad no haya sido aceptada por Terpel / Diners. </p>
+                <p>Terpel Ecuador se reserva el derecho de verificar la información proporcionada por los participantes y la autenticidad de la factura participante. Por lo tanto, no participarán de la promoción aquellas facturas que se encuentren dañadas, rotas, incompletas, con cualquier tipo de enmendadura externa y/o falsificados, y en general las facturas cuya autenticidad no haya sido aceptada por Terpel / Diners Club. </p>
                 <p>
                 A los ganadores se los contactará telefónicamente al número registrado por el participante dentro de los 05 (cinco) días hábiles subsiguientes a la fecha de realizada el sorteo para coordinar la entrega de los premios. El ganador tiene el término de 03 días hábiles contados desde que fue notificado para presentar los documentos necesarios que se requieran para poder acceder a su premio, en el caso de no presentarse el ganador principal se procederá a contactar a los ganadores suplentes y en caso de no presentarse estos o de no poder contactarlo dentro de los plazos antes indicados para la notificación y presentación se procederá a declarar desierto el premio.
                 </p>
@@ -155,13 +175,13 @@ $stations = [
                 </table>
                 <br>
                 <p>
-                Los Premios NO incluyen: Todo impuesto, tasa o contribución que deba tributarse, de conformidad con la legislación ecuatoriana, y asimismo, cualquier gasto que deba realizarse por concepto de impuestos, y/o toda suma de dinero que deba pagarse al Estado, sociedades del estado, provincias o municipalidades por el hecho del ofrecimiento, asignación o entrega de los premios o con motivo de la organización o difusión de la promoción o de los Sorteos y los gastos en que incurran sus Ganadores por cualquier concepto relacionado con la presente promoción será a exclusivo cargo de los mismos, así como todo otro servicio o bien que no fuera expresamente detallado en las presentes bases, los que de ocurrir serán a cargo de los Ganadores. 
+                Los Premios NO incluyen: Impuestos, tasa o contribución que deba tributarse, de conformidad con la legislación ecuatoriana, y asimismo, cualquier gasto que deba realizarse por concepto de impuestos, y/o toda suma de dinero que deba pagarse al Estado, sociedades del estado, provincias o municipalidades por el hecho del ofrecimiento, asignación o entrega de los premios o con motivo de la organización o difusión de la promoción o de los Sorteos y los gastos en que incurran sus Ganadores por cualquier concepto relacionado con la presente promoción será a exclusivo cargo de los mismos, así como todo otro servicio o bien que no fuera expresamente detallado en las presentes bases, los que de ocurrir serán a cargo de los Ganadores.
                 </p>
                 <p>
                 Terpel no se hace responsable de los gastos en los que incurra el ganador y sus acompañantes (viajes, hospedaje, viáticos, etc.) para efecto de reclamar el paquete.
                 </p>
                 <p>
-                Terpel no se hace responsable de cualquier gasto de alimentación o de transporte que incurra el ganador y/o sus acompañantes fuera de los descritos como parte del premio “Paquete Doble”.  
+                Terpel no se hace responsable de cualquier gasto de alimentación o de transporte que incurra el ganador y/o sus acompañantes fuera de los descritos como parte del premio. 
                 </p>
 
                 <p>
@@ -169,10 +189,10 @@ $stations = [
                 </p>
 
                 <p>
-                Terpel no se hace responsable de la integridad física, la salud, o la propiedad de los ganadores durante el evento o los desplazamientos asociados a éste, ni de cualquier otro daño que estos sufran con ocasión de la participación en la actividad o del disfrute del premio otorgado. Los ganadores entienden y aceptan lo anterior y, por tanto, exoneran a Terpel de cualquier reclamación judicial o extrajudicial que pudiere derivarse de tales eventos
+                Terpel no se hace responsable de la integridad física, la salud, o la propiedad de los ganadores durante el evento o los desplazamientos asociados a éste, ni de cualquier otro daño que estos sufran con ocasión de la participación en la actividad o del disfrute del premio otorgado. Los ganadores entienden y aceptan lo anterior, por tanto exoneran a Terpel de cualquier reclamación judicial o extrajudicial que pudiere derivarse de tales eventos.
                 </p>
 
-                <p>Los premios son personales e intransferibles y no canjeables. </p>
+                <p>Los premios son personales e intransferibles. </p>
 
                 <p>
                 Bajo ninguna circunstancia los ganadores podrán solicitar a Terpel que el premio al que se hacen acreedores en virtud de la Promoción sea entregado a una tercera persona ni aún para el caso de familiares.  
@@ -192,7 +212,7 @@ $stations = [
                     <li>Acudir personalmente al lugar, fecha y hora dispuesta por Terpel. </li>
                     <li>Presentar el original de su cédula de ciudadanía, pasaporte y entregar fotocopias a color  de estos documento. En caso de extranjeros residentes en Ecuador, presentar el original y una fotocopia de su cédula de identidad o pasaporte.</li>
                     <li>Presentar la factura participante. </li>
-                    <li>Presentar el voucher físico participante en caso de haber seleccionado pago con tarjetas participantes</li>
+                    <li>Presentar el voucher físico participante en caso de haber seleccionado pago con tarjetas participantes.</li>
                 </ul> 
                 </p>
 
@@ -252,7 +272,7 @@ $stations = [
                 El premio no es transferible, canjeable ni reembolsable por dinero en efectivo, sin excepciones.
                 </p>
                 <p>
-                El premio es una cortesía entregada por Terpel y de ninguna manera podrá ser revendido o distribuido a terceros. De hacerlo, el ganador deberá responder legalmente a Terpel  
+                El premio es una cortesía entregada por Terpel y de ninguna manera podrá ser revendido o distribuido a terceros. De hacerlo, el ganador deberá responder legalmente a Terpel.  
                 </p>
                 <p>
                 En ningún caso, se aceptarán registros con datos que no correspondan al titular de la cuenta, números incompletos o no válidos. Si los datos de registro no coinciden con el documento de identidad del ganador a la hora de hacer efectivo el premio, aquel no podrá recibirlo.
